62cc457bcf5dc06ae1fc09d73728a9472dda0023
Anthony Max <anthonymaxgit@gmail.com>
2024-11-12 21:25:48 +0300

    Update main.test.ts

--- test/main.test.ts
+++ test/main.test.ts
@@ -5,19 +5,19 @@ describe("compile function", () => {
   it("should throw error if template is not a string", () => {
     assert.throws(() => compile(123 as any), {
       message:
-        "Template was not found or the type of the passed value is not string"
+        "Compile error: Template was not found or the type of the passed value is not string"
     });
   });
 
   it("should throw error if template is empty", () => {
     assert.throws(() => compile(""), {
-      message: "Template must not be a falsey value"
+      message: "Compile error: Template must not be a falsey value"
     });
   });
 
   it("should throw error if options is not an object", () => {
     assert.throws(() => compile("some template", "invalid options" as any), {
-      message: "Options must be an object"
+      message: "Compile error: Options must be an object"
     });
   });
 });
